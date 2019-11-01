6996a25d85fd276c11c9f2629e6514db28403178
tianhaoyue <376656153@qq.com>
2019-11-01 19:38:26 +0800

    c++

--- day01_2019_11_1/day01_2019_11_1.cpp
+++ day01_2019_11_1/day01_2019_11_1.cpp
@@ -3,7 +3,7 @@
 
 
 #include <iostream>
-#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -66,12 +66,102 @@ int main()
 	cout << sum << endl;
 
 	cout << "输入一个整数,将各位数字反转后输出" << endl;
-	for (;;)
-	{
+	int n;
+	int right;
+	int enter;
+	cin >> enter;
 
+
+	for (n=enter;n!=0;n/=10)
+	{
+		right = n % 10;
+		cout << right;
+	}
+	cout << "\n";
+
+
+	cout << "输入一个整数，求出它的所有因子";
+	cout << "请输入一个整数：";
+	int enter1;
+	cin >> enter1;
+	int f;
+	for (f=1;f<enter1;f++)
+	{
+		if (enter1 % f == 0)
+		{
+			cout << f << "\t";
+		}
+		
+	}
+	cout << "\n";
+
+	cout << "求两自然数m , n的最大公约数";
+	cout << "\n";
+	cout << "请输入一个数m:";
+	int m;
+	cin >> m;
+	cout << "请输入一个数n:";
+	int nm;
+	cin >> nm;
+	int mn;
+	if (m < n) 
+	{
+		for (mn=m;mn>0;mn--)
+		{
+			if (m%mn==0 && nm%mn==0)
+			{
+				cout << mn <<endl;
+				break;
+			}
+		}
+	}
+	else
+	{
+		for (mn = nm; mn > 0; mn--)
+		{
+			if (m % mn == 0 && nm % mn == 0)
+			{
+				cout << mn << endl;
+				break;
+			}
+		}
+	}
+	cout << "\n";
+
+
+	cout << "求1-100以内的素数";
+	int g;
+	int h;
+	for (g=2;g<100;g++) 
+	{
+		for (h=2;h<=g;h++)
+		{
+			if (g % h == 0 && g!=h)
+			{
+				break;
+			}
+			if (g%h==0 && g==h)
+				cout << g << "\n";
+		}
+	}
+	cout << "\n";
+
+	cout << "鸡兔共有30只，脚共有90只，问鸡兔各有多少？";
+	int ji;
+	int tu;
+	for (ji=0;ji<=30;ji++)
+	{
+		for (tu=0;tu<=30-ji;tu++)
+		{
+			if (ji*2 + tu*4 ==90 && ji+tu == 30)
+			{
+				cout << "鸡：" << ji << "\n";
+				cout << "兔:" << tu << "\n";
+			}
+		}
 	}
 
-
+	cout << endl;
 
 
 }
