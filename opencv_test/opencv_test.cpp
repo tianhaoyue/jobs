b719b6e2d90b83d546638d97b71f8382befe6d69
tianhaoyue <376656153@qq.com>
2019-11-08 17:04:21 +0800

    opencv_test

---
+++ opencv_test/opencv_test.cpp
@@ -0,0 +1,31 @@
+﻿#include <opencv2/opencv.hpp>
+
+#include <iostream>
+
+using namespace cv;
+
+using namespace std;
+
+int main()
+
+{
+
+	Mat img = imread("D:\luna.jpg");
+
+	if (img.empty())
+
+	{
+
+		printf("Could not find the image!\n");
+
+		return -1;
+
+	}
+
+	imshow("ImputImage", img);
+
+	waitKey(0);
+
+	return 0;
+
+}
